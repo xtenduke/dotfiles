7bc1f51cd1238b86b5adb9a0f61c2732e808dbf7
Jake Laurie <jake.gordon.laurie@gmail.com>
2023-02-22 19:14:03 +1300

    Add vimplug install script

--- neovim/install.sh
+++ neovim/install.sh
@@ -3,3 +3,6 @@
 echo "Installing neovim config"
 
 ln -s "$(pwd)/nvim" ~/.config/nvim
+
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
