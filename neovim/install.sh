6392fe533cee8e18b2a9b3c38f5d6693399cd157
Jake Laurie <jake.gordon.laurie@gmail.com>
2023-03-13 20:51:57 +0800

    Fix neovim reinstall, ignore plugged dir

--- neovim/install.sh
+++ neovim/install.sh
@@ -2,6 +2,8 @@
 
 echo "Installing neovim config"
 
+# Remove old config...
+rm -rf ~/.config/nvim
 ln -s "$(pwd)/nvim" ~/.config/nvim
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
