c613f22c1c58a7ef3ab57dc3062bec92f1892599
Jake Laurie <jake@heyheron.ai>
2026-03-23 09:05:53 +1300

    Automatic read and write of buffers

--- neovim/nvim/lua/autocmds.lua
+++ neovim/nvim/lua/autocmds.lua
@@ -1,1 +1,23 @@
 require "nvchad.autocmds"
+
+-- Auto-reload files changed outside of nvim
+vim.opt.autoread = true
+vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold", "CursorHoldI" }, {
+  pattern = "*",
+  callback = function()
+    if vim.fn.mode() ~= "c" then
+      vim.cmd "checktime"
+    end
+  end,
+})
+
+-- Auto-save on focus lost or leaving insert mode
+vim.api.nvim_create_autocmd({ "InsertLeave", "FocusLost", "BufLeave" }, {
+  pattern = "*",
+  callback = function()
+    local buf = vim.api.nvim_get_current_buf()
+    if vim.bo[buf].modified and vim.bo[buf].buftype == "" and vim.fn.expand "%:t" ~= "" then
+      vim.cmd "silent! write"
+    end
+  end,
+})
